6f723df8196f051d51357bcedf87c819c192ca59
BadCuzMad <72146849+BadCuzMad@users.noreply.github.com>
2020-10-03 00:16:58 +0300

    Delete Book.java

--- Models/src/main/java/Book.java
+++
@@ -1,9 +0,0 @@
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-@Data
-class LBook {
-    @Setter
-    @Getter
-    private String bookproperty, qwerty, movie; //bname, genre, movie;
-}
